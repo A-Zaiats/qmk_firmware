ecbe1612489ccab6a1d0ed8e9795ac02dce762f2
Andrii Zaiats <andrei.zaiats@gmail.com>
2024-08-11 14:15:40 +0300

    Disable some features to reduce the compiled size

--- keyboards/cleo/v1/keymaps/A-Zaiats/keymap.c
+++ keyboards/cleo/v1/keymaps/A-Zaiats/keymap.c
@@ -1,8 +1,18 @@
 #include "../keymaps_base.c"
 
-void keyboard_post_init_user(void) {
-  debug_enable=true;
-  debug_matrix=true;
-//   debug_keyboard=true;
-//   debug_mouse=true;
+// void keyboard_post_init_user(void) {
+//   debug_enable=true;
+//   debug_matrix=true;
+//    debug_keyboard=true;
+//    debug_mouse=true;
+// }
+
+#ifndef MAGIC_ENABLE
+uint16_t keycode_config(uint16_t keycode) {
+    return keycode;
 }
+
+uint8_t mod_config(uint8_t mod) {
+    return mod;
+}
+#endif
